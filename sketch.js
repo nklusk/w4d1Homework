aa4600ecad99188eb138646d7d351bb0bb107a44
NATALIE LUSK <NKL22@fsu.edu>
2023-09-20 01:09:52 -0400

    animationAndTheMouse_task4

--- sketch.js
+++ sketch.js
@@ -1,31 +1,48 @@
 //global variables
-let xPosition;
-let speed = 4;
+let topCircleMovement;
+let bottomCircleMovement;
+let topSpeed = 4;
+let bottomSpeed = 4;
 let diameter = 40; 
 
 function setup() {
   createCanvas(400, 400)
   xPosition = width/2;
-
+  topCircleMovement = width/2
+  bottomCircleMovement = width/2
 }
 
 function draw() {
   background(200, 270, 23);
-  ellipse(xPosition, height/2, diameter);
 
-  xPosition += speed;
+  //bottom circle
+  ellipse(bottomCircleMovement, height/2, diameter);
 
-    if (xPosition >= width || xPosition <= 0) {
-    speed = speed * -1;
+
+  //top circle
+  ellipse(topCircleMovement, height/3, diameter/2);
+
+  topCircleMovement += topSpeed;
+
+    if (topCircleMovement >= width || topCircleMovement <= 0) {
+    topSpeed = topSpeed * -1;
 
   }
+
+  bottomCircleMovement += bottomSpeed;
+    
+    if (bottomCircleMovement >= width || bottomCircleMovement <=0) {
+        bottomSpeed = bottomSpeed * -1;
+    }
+   
+   
+    if (mouseIsPressed == true) {
+      bottomSpeed = 0
+    }
 
   }
 
   function mousePressed(){
-    loop();
-  }
-
-  function mouseReleased(){
-    noLoop();
+  
+  
   }
