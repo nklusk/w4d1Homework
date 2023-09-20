e35fa07a1ab414804c87e711722f02bbe6e692e0
NATALIE LUSK <NKL22@fsu.edu>
2023-09-20 02:29:23 -0400

    time_task1

--- sketch.js
+++ sketch.js
@@ -1,40 +1,19 @@
 //global variables
-let xPosition;
-let speed = 0;
-let diameter = 40; 
-let randomGray = 150
+
 
 function setup() {
   createCanvas(400, 400)
-  xPosition = width/2;
   
 }
 
 function draw() {
-  background(200, 270, 23);
-  
-  fill(randomGray);
-  
-  ellipse(xPosition, height/2, diameter);
-
-
-  xPosition += speed;
-
-    if (xPosition >= width || xPosition <= 0) {
-    speed = speed * -1;
-
+if (millis() > 10000) {
+  background(70, 200, 70)
+} else {
+  background(200, 50, 90)
+}
   }
+ 
 
-   if (mouseIsPressed == true){
-    
-   }
 
-   if (mouseIsPressed == true && speed == 0){
-    speed = 4;
-    randomGray = random(1,255);
-   } else if (mouseIsPressed == true){
-      speed = 0;
-   }
-   
-  
-  }
+
