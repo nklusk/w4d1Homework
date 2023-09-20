46f6ca1cea3592f330ad76ccd5acaa67a4bd41bc
NATALIE LUSK <NKL22@fsu.edu>
2023-09-20 00:16:45 -0400

    animationAndTheMouse_task2

--- sketch.js
+++ sketch.js
@@ -6,21 +6,33 @@ let diameter = 40;
 function setup() {
   createCanvas(400, 400)
   xPosition = width/2;
+  noLoop();
 
 }
+
+
 
 function draw() {
   background(200, 270, 23);
   ellipse(xPosition, height/2, diameter);
 
-xPosition += speed;
+  
 
-  if (xPosition >= width || xPosition <= 0) {
-  speed = speed * -1;
+
+
+
+
+
+  xPosition += speed;
+
+    if (xPosition >= width || xPosition <= 0) {
+    speed = speed * -1;
+
   }
 
-  if (mouseIsPressed == true) {
-    noLoop();
+ 
   }
 
-}
+  function mousePressed(){
+    loop();
+  }
